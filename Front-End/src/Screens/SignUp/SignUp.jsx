78b72add3a2c577d957345299edd2aa4cbd97543
m-ameer-hamza <ameerhamza01aug@gmail.com>
2025-01-13 01:01:50 +0500

    chanhed design of sign up page

--- Front-End/src/Screens/SignUp/SignUp.jsx
+++ Front-End/src/Screens/SignUp/SignUp.jsx
@@ -1,4 +1,11 @@
 import { useState, useEffect } from "react";
+import {
+  FaUserAlt,
+  FaEnvelope,
+  FaLock,
+  FaEye,
+  FaEyeSlash,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useUserApi } from "../../../customHooks/authApi";
@@ -6,6 +13,7 @@ import { useQuery } from "react-query";
 import { toast } from "react-toastify";
 import Loading from "../../components/Loading";
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
   const [emailError, setEmailError] = useState(true);
   const [passError, setPassError] = useState(true);
   const [nameError, setNameError] = useState(true);
@@ -72,103 +80,134 @@ function SignUp() {
           onSubmit={handleSubmit(onSubmit)}
         >
           <h3 className="card-title">Register</h3>
+
           {/* User Name Input */}
-          <div className="form-control">
+          <div className="form-control relative w-[100%]">
             <label className="label">
               <span className="label-text">User Name</span>
             </label>
-            {/* User Name Input Field */}
-            <input
-              type="text"
-              placeholder="Enter Name"
-              className={`input input-bordered ${
-                nameError ? "animate-shake" : ""
-              }`}
-              {...register("username", {
-                required: {
-                  value: true,
-                  message: "User name required",
-                },
-                pattern: {
-                  value: /^[a-zA-Z]+$/,
-                  message: "name contains only alphabets",
-                },
-                minLength: {
-                  value: 5,
-                  message: "name must be at least 5 characters",
-                },
-              })}
-            />
+            <div className="relative">
+              {/* Icon */}
+              <FaUserAlt
+                size={18}
+                className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-500"
+              />
+              {/* Input Field */}
+              <input
+                type="text"
+                placeholder="Enter Name"
+                className={`input input-bordered w-[100%] pl-14 ${
+                  nameError ? "animate-shake" : ""
+                }`}
+                {...register("username", {
+                  required: {
+                    value: true,
+                    message: "User name required",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z]+$/,
+                    message: "Name contains only alphabets",
+                  },
+                  minLength: {
+                    value: 5,
+                    message: "Name must be at least 5 characters",
+                  },
+                })}
+              />
+            </div>
             {errors.name && (
               <span className="text-red">{errors.name.message}</span>
             )}
           </div>
+
           {/* Email Input */}
-          <div className="form-control">
+          <div className="form-control relative w-[100%]">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            {/* Email Input Field */}
-            <input
-              type="email"
-              placeholder="Enter Email"
-              className={`input input-bordered ${
-                emailError ? "animate-shake" : ""
-              }`}
-              {...register("email", {
-                required: {
-                  value: true,
-                  message: "email required",
-                },
-                pattern: {
-                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                  message: "invalide email",
-                },
-              })}
-            />
+            <div className="relative">
+              {/* Icon */}
+              <FaEnvelope
+                size={18}
+                className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-500"
+              />
+              {/* Input Field */}
+              <input
+                type="email"
+                placeholder="Enter Email"
+                className={`input input-bordered w-[100%] pl-14 ${
+                  emailError ? "animate-shake" : ""
+                }`}
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: "Email required",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Invalid email",
+                  },
+                })}
+              />
+            </div>
             {errors.email && (
               <span className="text-red">{errors.email.message}</span>
             )}
           </div>
+
           {/* Password Input */}
-          <div className="form-control">
+          <div className="form-control relative w-[100%]">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            {/* Password Input Field */}
-            <input
-              type="password"
-              placeholder="Enter Password"
-              className={`input input-bordered ${
-                errors.password ? "animate-shake" : ""
-              }`}
-              {...register("password", {
-                required: {
-                  value: true,
-                  message: "password required",
-                },
-                minLength: {
-                  value: 8,
-                  message: "password must be at least 8 characters",
-                },
-                pattern: {
-                  value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
-                  message:
-                    "password must contain upper-case, lower-case and number",
-                },
-              })}
-            />
+            <div className="relative">
+              {/* Icon */}
+              <FaLock
+                size={20}
+                className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-500"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 focus:outline-none"
+              >
+                {showPassword ? <FaEyeSlash size={21} /> : <FaEye size={21} />}
+              </button>
+              {/* Input Field */}
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter Password"
+                className={`input input-bordered w-[100%] pl-14 ${
+                  errors.password ? "animate-shake" : ""
+                }`}
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password required",
+                  },
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters",
+                  },
+                  pattern: {
+                    value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+                    message:
+                      "Password must contain upper-case, lower-case, and number",
+                  },
+                })}
+              />
+            </div>
             {errors.password && (
               <span className="text-red">{errors.password.message}</span>
             )}
           </div>
+
           <div className="form-control mt-6">
             <input
               title="Register"
               type="submit"
               value="Register"
               className="btn bg-green text-white"
-              // disabled={isLoading}
             />
           </div>
           <p className="text-center my-3">
